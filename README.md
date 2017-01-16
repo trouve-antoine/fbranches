2ff2b393b347f83bcef71b121b7526554889d204
Antoine TROUVE <trouve@roz-aibod.local>
2017-01-16 15:29:27 +0900

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # fbranches
 functional version of javascript's switch and if statements
+
+## `if` statement
+
+```
+if(k==k1) { return v1 }
+else { return v2 }
+```
+
+becomes
+
+```
+import { fif } from 'fbranches'
+fif(k1)
+  .then(v1)
+  .else(v2)
+  .exec(k)
+```
+
+## `switch` statement
+
+```
+switch(k) {
+  case k1:
+  	return v1;
+  case k2:
+    return v2;
+  default:
+    return v3
+}
+```
+
+becomes
+
+```
+import { fswitch } from 'fbranches'
+fswitch()
+  .fcase(k1, v1)
+  .fcase(k2, v2)
+  .fdefault(v2)
+  .exec(k)
+```
+
+## Function variants
+
+All `fthen`, `felse`, `fcase` and `fdefault` have a variant, postfixed by `_f`, that accept a function instead of v. The arguments to the function may be passed in the exec function.
+
+For example, with `fif`:
+
+```
+import { fif } from 'fbranches'
+fif(k1)
+  .then_f(f1)
+  .else(v2)
+  .exec(k, a1, a2)
+```
+
+The `exec` function would call `f1(a1, a2)` if `k==k1`, or return `v2` otherwise.
---
+++ index.js
@@ -0,0 +1,93 @@
+const ensure_function = (v) => {
+  if(typeof(v) !== 'function') { throw new Error("Unexpected value of type" + typeof(v) + ": expected a function") }
+}
+
+export const fswitch = function() {
+  const the_cases = []
+  let the_default = {}
+
+  const fcase = (k, v) => {
+    the_cases.push({ k, v })
+    return that
+  }
+
+  const fcase_f = (k, v) => {
+    ensure_function(v)
+    the_cases.push({ k, v, exec: true })
+    return that
+  }
+
+  const fdefault = (v) => {
+    the_default = { v }
+    return that
+  }
+
+  const fdefault_f = (v) => {
+    ensure_function(v)
+    the_default = { v, exec: true }
+    return that
+  }
+
+  const exec = (k, ...otherArgs) => {
+    const value = (kv) => {
+      if(kv.exec) { return kv.v(...otherArgs) }
+      else { return kv.v }
+    }
+
+    for(let i=0; i<the_cases.length; i++) {
+      const kv = the_cases[i]
+      if(kv.k == k) { return value(kv) }
+    }
+
+    return value(the_default)
+  }
+
+  const that = { fcase, fcase_f, fdefault, fdefault_f, exec }
+
+  return that
+}
+
+export const fif = function(k) {
+  const the_k = k
+  let then_kv = {}
+  let else_kv = {}
+
+  const fthen = (v) => {
+    then_kv = { v }
+    return that
+  }
+
+  const fthen_f = (v) => {
+    ensure_function(v)
+    then_kv = { v, exec: true }
+    return that
+  }
+
+  const felse = (v) => {
+    else_kv = { v }
+    return that
+  }
+
+  const felse_f = (v) => {
+    ensure_function(v)
+    else_kv = { v, exec: true }
+    return that
+  }
+
+  const exec = (k, ...otherArgs) => {
+    const value = (kv) => {
+      if(kv.exec) { return kv.v(...otherArgs) }
+      else { return kv.v }
+    }
+
+    if(k==the_k) {
+      return value(then_kv)
+    } else {
+      return value(else_kv)
+    }
+  }
+
+  const that = { fthen, fthen_f, felse, felse_f, exec }
+
+  return that
+}
